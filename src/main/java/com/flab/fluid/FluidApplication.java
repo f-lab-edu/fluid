6bdd359ce0c5da9b14be63f39b3bd03ab1bca373
num22yh <num22yh@gmail.com>
2025-07-09 23:24:01 +0900

    initial commit

---
+++ src/main/java/com/flab/fluid/FluidApplication.java
@@ -0,0 +1,13 @@
+package com.flab.fluid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flab/fluid/FluidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.fluid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
