83237da2222c7d7250bfb23add7a22de16cba66d
num22yh <num22yh@naver.com>
2025-08-17 17:27:14 +0900

    refactor : 단일 모듈로 초기화

---
+++ src/main/java/fluid/FluidApplication.java
@@ -0,0 +1,12 @@
+package fluid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FluidApplication.class, args);
+    }
+}
